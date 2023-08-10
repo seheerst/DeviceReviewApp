a3ef43922c97fb9c312c16e36f08630a306c5fd9
seheerst <seherst9@gmail.com>
2023-08-10 18:01:26 +0300

    feat: added list filter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using DeviceReviewApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DeviceReviewApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(string searchString)
+    
+        public IActionResult Index(string searchString, string category)
         {
             var products = Repository.Products;
             
@@ -15,6 +17,13 @@ namespace DeviceReviewApp.Controllers
                 ViewBag.SearchString = searchString;
                 products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
             }
+
+            if (!String.IsNullOrEmpty(category) && int.Parse(category) != 0)
+            {
+                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+            }
+
+            ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId", "Name", category);
             return View(products);
         }
 

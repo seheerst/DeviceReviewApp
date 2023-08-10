faaeed2678e80d24569b74b77342515f80aa5b95
seheerst <seherst9@gmail.com>
2023-08-10 22:36:41 +0300

    feat: ProductViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,8 +23,16 @@ namespace DeviceReviewApp.Controllers
                 products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
             }
 
-            ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId", "Name", category);
-            return View(products);
+            // ViewBag.Categories = new SelectList(Repository.Categories,"CategoryId", "Name", category);
+
+            var model = new ProductViewModel()
+            {
+                Products = products,
+                Categories = Repository.Categories,
+                SelectedCategory = category
+                    
+            };
+            return View(model);
         }
 
         public IActionResult Privacy()
